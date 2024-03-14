7aaf93c1efa4c837a9f9eaf7b793c0016991e402
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2024-03-14 19:07:26 +0200

    Create index.py

---
+++ index.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Blueprint, render_template;
+app = Flask(__name__);
+DEBUG=False;
+#Registramos una nueva url
+media_pt=Blueprint("media", __name__, static_folder="./media", static_url_path="/media");
+app.register_blueprint(media_pt);
+
+media_pt=Blueprint("static", __name__, static_folder="./static", static_url_path="/static");
+app.register_blueprint(media_pt);
+
+@app.route("/")
+def index():
+    return render_template("index.html");
+@app.route("/ok")
+def is_of():
+    return "is okk";
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=DEBUG)
